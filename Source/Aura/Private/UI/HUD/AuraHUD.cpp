668d3a7524339eeee0d5edc7505d225012448549
Timur Djandjgava <dj.timur.work@gmail.com>
2026-02-12 20:23:52 +0500

    AuraHUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+// Copyright Shavi
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* UserWidget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	UserWidget->AddToViewport();
+}
