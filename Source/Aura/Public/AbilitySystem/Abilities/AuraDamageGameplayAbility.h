3d8fbb9dbf2d516e2c64928a74b4bd6f07c7dda6
Timur Djandjgava <dj.timur.work@gmail.com>
2026-04-03 16:52:08 +0500

    Damage Types

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
@@ -0,0 +1,24 @@
+// Copyright Shavi
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraDamageGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraDamageGameplayAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	TMap<FGameplayTag, FScalableFloat> DamageTypes;
+};
