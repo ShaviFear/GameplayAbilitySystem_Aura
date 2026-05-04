342d7c23ceb42129d8a4c7ee1a22dd870bf9772b
Timur Djandjgava <dj.timur.work@gmail.com>
2026-05-04 18:59:26 +0500

    Goblin Spear - Hurt and Death Sounds

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -101,6 +101,9 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	UNiagaraSystem* BloodEffect;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	USoundBase* DeathSound;
+
 private:
 	UPROPERTY(EditAnywhere, Category = "Abilites")
 	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;
